cd2eeb96ba0baaefe21ad3fa353a4452218d556b
Kamil Matuszewski <kmatuszewski2310@gmail.com>
2023-10-08 14:51:32 +0200

    toggling dark and light mode implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import {useState} from "react";
 import Nav from "./components/Nav";
 import Main from "./components/Main";
 
 export default function App() {
+  const [darkMode, setDarkMode] = useState(true);
+
+  const toggleMode = () => {
+    setDarkMode(prev => !prev);
+  }
+
   return (
     <div className="container">
-      <Nav />
-      <Main />
+      <Nav darkMode={darkMode} toggleDarkMode={toggleMode} />
+      <Main darkMode={darkMode} />
     </div>
   )
 }
